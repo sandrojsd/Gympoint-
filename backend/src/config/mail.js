97061d52038ca55182b352bd84d1ba591736ce0b
Alexsandro Trindade <sandrojsd@gmail.com>
2020-02-09 15:16:03 -0400

    adicionado o controle de tratamento de erros

--- backend/src/config/mail.js
+++ backend/src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '430ddac6d75b5e',
-    pass: '956d5f97b7b5f2',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PORT,
   },
   default: {
     from: 'Equipe Gympoint <nopley@gympoint.com',
--- backend/src/config/sentry.js
+++ backend/src/config/sentry.js
@@ -1,5 +1,4 @@
 export default {
-  // 'https://94c5231c8b1348f8ad2b47d3d91ce931@sentry.io/1869488'
-  // dsn: process.env.SENTRY_DSN,
-  dns: 'https://7310349972cc4d1fb5bb7708dc26d7a4@sentry.io/2337784',
+  // 'https://7310349972cc4d1fb5bb7708dc26d7a4@sentry.io/2337784',
+  dns: process.env.SENTRY_DSN,
 };
