ed3235250139ece429c1ccf9b6c9f275a8d20125
Alexsandro Trindade <sandrojsd@gmail.com>
2020-02-09 15:01:14 -0400

    adicioado o tratamanento de erros

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,8 @@
 import express from 'express';
+import * as Sentry from '@sentry/node';
+import Youch from 'youch';
+import 'express-async-errors';
+import sentryConfig from './config/sentry';
 import routes from './routes';
 
 import './database';
@@ -7,16 +11,34 @@ class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.midedlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   midedlewares() {
+    this.server.unlock(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
   }
 
   routes() {
     this.server.use(routes);
+    // o midedlewar do sentry tem que está depois de todas as rotas
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (error, req, res, next) => {
+      if (process.env.NODE_ENV === 'development') {
+        const errors = await new Youch(error, req).toJSON();
+
+        return res.status(500).json(errors);
+      }
+
+      return res.status(500).json({ error: 'Internal Server Error' });
+    });
   }
 }
 
